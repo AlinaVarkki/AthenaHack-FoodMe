25ded142581f18a9651a644506f856674dffb60d
alinavarkki <alina@viaeurope.com>
2022-10-22 18:46:04 +0100

    Added a map and switch between list and map view

--- Views/FeedScreen.js
+++ Views/FeedScreen.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-import { StyleSheet, View, Text } from "react-native";
+import {StyleSheet, View, Text, SafeAreaView, StatusBar} from "react-native";
+import CommunityScreen from "./CommunityScreen";
+import ImpactScreen from "./ImpactScreen";
+import BottomMenuButton from "../Components/BottomMenuButton";
+import ColorPalette from "../assets/ColorPalette";
+import OffersMap from "../homeScreenComponents/OffersMap";
+import MapView from "react-native-maps";
+import OffersList from "../homeScreenComponents/OffersList";
+import UpperMenuButton from "../Components/UpperMenuButton";
 
 
 const FeedScreen = () => {
+    const [chosen, changeChosen] = useState("ListView");
+
+    const content = () => {
+        switch (chosen) {
+            case "ListView":
+                return (<OffersList/>);
+            default:
+                return (<OffersMap/>);
+        }
+    }
+
     return (
-    <View style={styles.container}>
-        <Text>Feed</Text>
-      </View>
-      );
+        <>
+            <View style={styles.topNav}>
+                    <UpperMenuButton name="home" text="List" callback={()=>changeChosen("ListView")} color={chosen === "ListView" ? ColorPalette.darkOrange : ColorPalette.darkgrey}/>
+                    <UpperMenuButton name="home" text="Map" callback={()=>changeChosen("MapView")} color={chosen === "MapView" ? ColorPalette.darkOrange : ColorPalette.darkgrey}/>
+            </View>
+                {content()}
+        </>
+    )
+
 };
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
+    topNav:{
+        height: '12.5%',
+        width: '100%',
+        alignItems: 'stretch',
+        borderBottomWidth: 1,
+        borderColor: ColorPalette.darkOrange,
+        borderTopColor: 'grey',
+        flexDirection: 'row',
     },
-  });
-
+});
 export default FeedScreen;
---
+++ homeScreenComponents/OffersList.js
@@ -0,0 +1,7 @@
+import {Text} from "react-native";
+
+export default function OffersList() {
+    return (
+        <Text>List</Text>
+    )
+}
