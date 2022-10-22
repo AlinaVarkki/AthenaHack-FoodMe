44947e2d7299d07783b9e7c6b0e5e7952e094ef9
alinavarkki <alina@viaeurope.com>
2022-10-22 19:11:53 +0100

    changed menu style

--- assets/ColorPalette.js
+++ assets/ColorPalette.js
@@ -1,7 +1,8 @@
 export default {
     darkgrey: '#222222',
     offwhite: '#F5F5F5',
-    orange: '#FFAC00',
+    white: '#ffffff',
+    orange: '#FF7900',
     blue: '#065CF8',
     lightYellow: '#FFF2D8',
     darkOrange: '#cd8c00',
